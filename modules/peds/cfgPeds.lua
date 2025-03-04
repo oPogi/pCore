5126b5f2d5873d92c7697c7e812c5f41467d02bb
Pogi <88253567+oPogi@users.noreply.github.com>
2025-03-04 06:19:37 -0500

    Update cfgPeds.lua

--- modules/peds/cfgPeds.lua
+++ modules/peds/cfgPeds.lua
@@ -6,6 +6,13 @@ return {
             Scenario = "WORLD_HUMAN_AA_SMOKE",
             AnimationName = nil,
             AnimationDict = nil,
-        }
+        },
+       -- {
+       --     Coords = vector4(325.00, -1374.32, 31.90, 51.02),
+       --     Model = "a_m_y_motox_01",
+       --     Scenario = "WORLD_HUMAN_AA_SMOKE",
+       --     AnimationName = nil,
+       --     AnimationDict = nil,
+       -- }
     }
 }
